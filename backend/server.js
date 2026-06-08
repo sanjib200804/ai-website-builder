18a0abd566c3c518e704744495ca943ccd87e3b7
Sanjib Garu <sanjib2019garu@gmail.com>
2026-06-08 13:01:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,11 +31,9 @@ app.use('/api/auth', router)
 app.use('/api/user', userRouter)
 
 app.use('/api/website', webRouter)
-app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname,'../frontend' ,'dist', 'index.html'))
-})
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
